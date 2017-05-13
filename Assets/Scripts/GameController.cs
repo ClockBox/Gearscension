92b655ad926a8a4e0620a8d7f34c9024a80b98dd
MSI\easyhome <strdircorvinus@gmail.com>
2017-05-13 14:55:05 -0400

    disley

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+
+    public void LoadByIndex(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+
+	// Use this for initialization
+	public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+}
+
