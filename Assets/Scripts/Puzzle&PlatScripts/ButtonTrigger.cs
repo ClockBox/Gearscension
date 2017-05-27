0f932b4b10b63b05738e631216f293a89671036e
DESKTOP-2IU9LH4\iDonChilito <kenneth.striker@mytfs.ca>
2017-05-27 09:27:28 -0400

    Week_6

--- Assets/Scripts/Puzzle&PlatScripts/ButtonTrigger.cs
+++ Assets/Scripts/Puzzle&PlatScripts/ButtonTrigger.cs
@@ -8,23 +8,13 @@ public class ButtonTrigger : MonoBehaviour
     public UnityEvent myEvent;
 
     SlidingPlatform sP;
-    RotatingPuzzle[] rP;
     
-    public void RotatePlatfrom(GameObject platformsParent)
-    {
-        rP = platformsParent.GetComponentsInChildren<RotatingPuzzle>();
-        for (int i = 0; i < rP.Length; i++)
-        {
-            rP[i].Rotate();
-        }
-    }
-
     public void MovePlatform(GameObject platform)
     {
         sP = platform.GetComponent<SlidingPlatform>();
         sP.move = true;
     }
-
+    
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Player")
