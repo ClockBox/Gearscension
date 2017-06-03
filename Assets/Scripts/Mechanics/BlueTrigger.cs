0228a51393efe1c192c072403285d1cbe03db1a3
DESKTOP-2IU9LH4\iDonChilito <kenneth.striker@mytfs.ca>
2017-06-03 10:17:23 -0400

    Light puzzle complete.

---
+++ Assets/Scripts/Mechanics/BlueTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueTrigger : MonoBehaviour
+{
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                gameObject.GetComponentInParent<LightPuzzle>().Invoke("BlueTrigger", 0.1f);
+            } 
+        }
+    }
+}
