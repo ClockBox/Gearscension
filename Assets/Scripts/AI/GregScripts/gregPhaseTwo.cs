cb90856fb95e6e1b21abd5feb7e12234601fd862
James Shen <jamesshen03@gmail.com>
2017-06-10 13:47:49 -0400

    changes to boss

--- Assets/Scripts/AI/GregScripts/gregPhaseTwo.cs
+++ Assets/Scripts/AI/GregScripts/gregPhaseTwo.cs
@@ -4,5 +4,37 @@ using UnityEngine;
 
 public class gregPhaseTwo : MonoBehaviour {
 
-   
+	gregAttacks attackScript;
+	public float attackCD = 4f;
+	GameObject player;
+	Vector3 point;
+	public float speed = 1f;
+	Animator anim;
+	private void Start()
+	{
+
+		player = GameObject.FindGameObjectWithTag("Player");
+		attackScript = GetComponent<gregAttacks>();
+		anim = GetComponent<Animator>();
+		anim.SetBool("phase2", true);
+
+	}
+	void Update()
+	{
+
+
+		if (attackCD >= 4f && anim.GetCurrentAnimatorStateInfo(0).IsName("idle"))
+		{
+			attackScript.chooseAttack2();
+
+			attackCD = 0f;
+		}
+		else
+		{
+			attackCD += Time.deltaTime;
+		}
+		point = player.transform.position;
+		point.y = transform.position.y;
+		transform.LookAt(point);
+	}
 }
