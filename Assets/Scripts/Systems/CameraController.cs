63acdf95ce9e681a2b359d88d152ffd7bd271dca
Ben Willemsma <ben.willemsma@gmail.com>
2017-06-17 09:21:47 -0400

    whiteBox for Floor 1 & 3

--- Assets/Scripts/Systems/CameraController.cs
+++ Assets/Scripts/Systems/CameraController.cs
@@ -21,46 +21,43 @@ public class CameraController : MonoBehaviour
     {
         if (!camPivot)
             camPivot = GameObject.FindGameObjectWithTag("Player").transform.GetChild(0);
+        //Mouse Input
+        currentX += Input.GetAxis("Mouse X") * sensitivity;
+        currentY -= Input.GetAxis("Mouse Y") * sensitivity;
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+
+        userDistance -= Input.GetAxis("Zoom") * 2;
+        userDistance = Mathf.Clamp(userDistance, 2.5f, 6);
+
+        //Zoom
+        if (m_zoomed)
+        {
+            distance = Mathf.MoveTowards(1.5f, userDistance, Time.deltaTime * 0.5f);
+            GetComponent<Camera>().fieldOfView = zoom;
+        }
         else
         {
-            //Mouse Input
-            currentX += Input.GetAxis("Mouse X") * sensitivity;
-            currentY -= Input.GetAxis("Mouse Y") * sensitivity;
-            currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
-
-            userDistance -= Input.GetAxis("Zoom") * 2;
-            userDistance = Mathf.Clamp(userDistance, 2.5f, 6);
-
-            //Zoom
-            if (m_zoomed)
-            {
-                distance = Mathf.MoveTowards(1.5f, userDistance, Time.deltaTime * 0.5f);
-                GetComponent<Camera>().fieldOfView = zoom;
-            }
-            else
-            {
-                distance = Mathf.MoveTowards(userDistance, 1.5f, Time.deltaTime * 0.5f);
-                GetComponent<Camera>().fieldOfView = normal;
-            }
-
-            //Set Position and Rotation
-            Vector3 moveDirection = new Vector3(0, 0, distance);
-            Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
-            transform.position = camPivot.position + rotation * moveDirection;
-            transform.LookAt(camPivot.position);
-
-            //Camera Raycast
-            RaycastHit hit;
-            if (Physics.SphereCast(camPivot.position, 0.2f, transform.position - camPivot.position, out hit, distance))
-            {
-                if (hit.transform.root.gameObject != camPivot.parent.gameObject)
-                    transform.position = hit.point + hit.normal * 0.2f;
-            }
-
-            //Check distance to player, move camera if to close
-            if ((transform.position - camPivot.position).magnitude < 1f)
-                currentY -= 5f;
+            distance = Mathf.MoveTowards(userDistance, 1.5f, Time.deltaTime * 0.5f);
+            GetComponent<Camera>().fieldOfView = normal;
         }
+
+        //Set Position and Rotation
+        Vector3 moveDirection = new Vector3(0, 0, distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        transform.position = camPivot.position + rotation * moveDirection;
+        transform.LookAt(camPivot.position);
+
+        //Camera Raycast
+        RaycastHit hit;
+        if (Physics.SphereCast(camPivot.position, 0.2f, transform.position - camPivot.position, out hit, distance))
+        {
+            if (hit.transform.root.gameObject != camPivot.parent.gameObject)
+                transform.position = hit.point + hit.normal * 0.2f;
+        }
+
+        //Check distance to player, move camera if to close
+        if ((transform.position - camPivot.position).magnitude < 1f)
+            currentY -= 5f;
     }
 
     public static bool Zoomed
