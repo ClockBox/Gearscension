4b32e6ce7d26396028d099cab65d9f099ba1e3a9
PhilipMarian <philmarian69@gmail.com>
2017-10-05 14:11:50 -0400

    First sound mixers and audio files.

---
+++ Assets/Scripts/Audio/SoundLevels.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundLevels : MonoBehaviour {
+
+    public AudioMixer masterMixer;
+
+    public void SetMasterVol(float vol)
+    {
+        masterMixer.SetFloat("masterVol", vol);
+    }
+
+    public void SetMusicVol(float vol)
+    {
+        masterMixer.SetFloat("musicVol", vol);
+    }
+
+    public void SetSFXVol(float vol)
+    {
+        masterMixer.SetFloat("sfxVol", vol);
+    }
+
+    public void SetAmbienceVol(float vol)
+    {
+        masterMixer.SetFloat("ambienceVol", vol);
+    }
+
+    public void SetVoiceVol(float vol)
+    {
+        masterMixer.SetFloat("voiceVol", vol);
+    }
+
+}
