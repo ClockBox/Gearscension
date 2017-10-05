ace5f54c489340df1fc9c6af1f44f16b867a4bf3
MSI\KageStriker <kenneth.striker@mytfs.ca>
2017-10-04 20:33:54 -0400

    puzzle manager

--- Assets/Scripts/Mechanics/PuzzleManager.cs
+++ Assets/Scripts/Mechanics/PuzzleManager.cs
@@ -2,25 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Axis
+{
+    X,
+    Y,
+    Z
+}
+
+
 public class PuzzleManager : MonoBehaviour
 {
     private float rotateSpeed;
     public bool allowRotate;
     private Vector3 right;
     private bool rotated = false;
+    private bool breakout = false;
+    public float angleRotated;
     private float moveSpeed = 0.1f;
-    Quaternion startPos;
+    private Vector3 rotateAngle;
+    private float toAngle;
+    public Axis axis;
     
     public void TrapDoor(GameObject thingToRotate)
     {
         StartCoroutine(OpenDoor(thingToRotate));
     }
 
-    public void RotateRoom(GameObject thingToRotate)
-    {
-        StartCoroutine(Rotate(thingToRotate));
-    }
-    
     public IEnumerator OpenDoor(GameObject thingToRotate)
     {
         if (!rotated)
@@ -51,22 +58,74 @@ public class PuzzleManager : MonoBehaviour
         }
     }
 
+    public void RotateRoom(GameObject thingToRotate)
+    {
+        StartCoroutine(Rotate(thingToRotate));
+    }
 
     public IEnumerator Rotate(GameObject thingToRotate)
     {
-        Debug.Log("Start");
         moveSpeed = 0;
-        startPos = thingToRotate.transform.rotation;
+
         while (thingToRotate.transform.rotation.x > 0)
         {
             thingToRotate.transform.rotation = Quaternion.Slerp(thingToRotate.transform.rotation, Quaternion.Euler(0, 0, 0), moveSpeed * Time.deltaTime);
             moveSpeed += Time.deltaTime / 10;
-            Debug.Log("Rotating");
             yield return null;
         }
-        Debug.Log("End");
 
         thingToRotate.transform.rotation = Quaternion.Euler(0, 0, 0);
         yield return null;
+    }
+
+    public void SetAxis(string _axis)
+    {
+        switch (_axis)
+        {
+            case "X":
+                axis = Axis.X;
+                break;
+            case "Y":
+                axis = Axis.Y;
+                break;
+            case "Z":
+                axis = Axis.Z;
+                break;
+        }
+    }
+
+    public void SetAngle(float angle)
+    {
+        toAngle = angle;
+    }
+
+    public void RotateObject(Transform thingToRotate)
+    {
+        StartCoroutine(RotateObj(thingToRotate));
+    }
+
+    private IEnumerator RotateObj(Transform thingToRotate)
+    {
+        angleRotated = 0;
+        while(angleRotated < toAngle)
+        {
+            Debug.Log(angleRotated);
+            switch (axis)
+            {
+                case Axis.X:
+                    thingToRotate.Rotate(2f * Time.deltaTime, 0, 0);
+                    break;
+                case Axis.Y:
+                    thingToRotate.Rotate(0, 2f * Time.deltaTime, 0);
+                    break;
+                case Axis.Z:
+                    thingToRotate.Rotate(0, 0, 2f * Time.deltaTime);
+                    break;
+            }
+            angleRotated += 2f * Time.deltaTime;
+            yield return null;
+        }
+        thingToRotate.localEulerAngles = new Vector3(axis == Axis.X ? toAngle : 0, axis == Axis.Y ? toAngle : 0, axis == Axis.Z ? toAngle : 0);
+        
     }
 }
