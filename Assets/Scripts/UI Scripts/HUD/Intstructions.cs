f61615ba41eb382f8cb23b57f6e7b4dbac2d4448
MSI\easyhome <strdircorvinus@gmail.com>
2017-11-24 13:59:56 -0500

    blah blah

---
+++ Assets/Scripts/UI Scripts/HUD/Intstructions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intstructions : MonoBehaviour
+{
+    PlayerHud PH;
+
+    // String
+    public string passedString;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PH.AddDisplay(passedString);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        PH.RemoveDisplay();
+    }
+}
