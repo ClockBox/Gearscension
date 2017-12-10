6405b772079f5a1e7d3203ac8f5fce08ca4827c5
DESKTOP-2IU9LH4\iDonChilito <k.striker23@hotmail.com>
2017-12-10 14:54:56 -0500

    killbox added

---
+++ Assets/Scripts/Mechanics/KillBox.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillBox : MonoBehaviour
+{
+    private GameManager gM = FindObjectOfType<GameManager>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            gM.RespawnPlayer();
+            return;
+        }
+
+        Destroy(other.gameObject);
+    }
+}
